8beeb188a86ec4b2bb5e1408e5ce63b5a84f650c
Cihan Bilgihan <94874242+tynes0@users.noreply.github.com>
2025-11-21 23:12:07 +0300

    Update 3.0
    Refactor logging color handling for cross-platform support and update license information.

--- dtlog.cpp
+++ dtlog.cpp
@@ -1,10 +1,10 @@
-﻿/*
+/*
  * This file is part of the dtlog library, originally created by Tynes0.
  * For the latest version and updates, please visit the official dtlog GitHub repository:
  * https://github.com/tynes0/dtlog
  *
  * dtlog is a basic library for logging, providing fast and user-friendly use
- * It is released under the Apache License 2.0. See the LICENSE file in the root of the dtlog repository
+ * It is released under the MIT License. See the LICENSE file in the root of the dtlog repository
  * or visit the above GitHub link for more details.
  *
  * For contributions, bug reports, or other inquiries, feel free to contact the author:
@@ -13,144 +13,111 @@
  */
 
 #include "dtlog.h"
+#include <cstdio>
 
 #ifdef _WIN32
-
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
-#endif // WIN32_LEAN_AND_MEAN
+#endif
 #include <Windows.h>
 
-void dtlog::logger::set_stdout_color(log_level level)
+namespace
 {
-	WORD color_code = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+    WORD get_win_color_code(dtlog::log_level level)
+    {
+        switch (level)
+        {
+        case dtlog::log_level::info:     return FOREGROUND_GREEN;
+        case dtlog::log_level::debug:    return FOREGROUND_BLUE | FOREGROUND_INTENSITY; 
+        case dtlog::log_level::warning:  return FOREGROUND_RED | FOREGROUND_GREEN; 
+        case dtlog::log_level::error:    return FOREGROUND_RED;
+        case dtlog::log_level::critical: return BACKGROUND_RED | FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY;
+        case dtlog::log_level::trace:    return FOREGROUND_INTENSITY; 
+        case dtlog::log_level::none:
+            break;
+        }
+        return FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE; 
+    }
 
-	switch (level)
-	{
-	case log_level::none:
-	case log_level::trace:
-		break;
-	case log_level::info:
-		color_code = FOREGROUND_GREEN;
-		break;
-	case log_level::debug:
-		color_code = FOREGROUND_BLUE;
-		break;
-	case log_level::warning:
-		color_code = FOREGROUND_RED | FOREGROUND_GREEN;
-		break;
-	case log_level::error:
-		color_code = FOREGROUND_RED;
-		break;
-	case log_level::critical:
-		color_code = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | FOREGROUND_RED;
-		break;
-	default:
-		break;
-	}
+    void set_console_color(DWORD handle, dtlog::log_level level)
+    {
+        HANDLE hConsole = GetStdHandle(handle);
+        if (hConsole != INVALID_HANDLE_VALUE) {
+            SetConsoleTextAttribute(hConsole, get_win_color_code(level));
+        }
+    }
 
-	HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
-	if (console_handle == INVALID_HANDLE_VALUE)
-		throw std::invalid_argument("INVALID STD HANDLE (logger::set_stdout_color())");
-	SetConsoleTextAttribute(console_handle, color_code);
+    void reset_console_color(DWORD handle)
+    {
+        HANDLE hConsole = GetStdHandle(handle);
+        if (hConsole != INVALID_HANDLE_VALUE) {
+            SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+        }
+    }
 }
 
-void dtlog::logger::set_stderr_color(log_level level)
+#else // (Linux, macOS, Unix)
+
+#include <cstring>
+
+namespace
 {
-	WORD color_code = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
-
-	switch (level)
-	{
-	case log_level::none:
-	case log_level::trace:
-		break;
-	case log_level::info:
-		color_code = FOREGROUND_GREEN;
-		break;
-	case log_level::debug:
-		color_code = FOREGROUND_BLUE;
-		break;
-	case log_level::warning:
-		color_code = FOREGROUND_RED | FOREGROUND_GREEN;
-		break;
-	case log_level::error:
-		color_code = FOREGROUND_RED;
-		break;
-	case log_level::critical:
-		color_code = BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE | FOREGROUND_RED;
-		break;
-	default:
-		break;
-	}
-
-	HANDLE console_handle = GetStdHandle(STD_ERROR_HANDLE);
-	if (console_handle == INVALID_HANDLE_VALUE)
-		throw std::invalid_argument("INVALID STD HANDLE (logger::set_stderr_color())");
-	SetConsoleTextAttribute(console_handle, color_code);
-}
-#else // _WIN32
-
-
-void dtlog::logger::set_stdout_color(log_level level)
-{
-	const char* color_code = "\x1b[0m";
-
-	switch (level)
-	{
-	case log_level::none:
-	case log_level::trace:
-		break;
-	case log_level::info:
-		color_code = "\x1b[32m";
-		break;
-	case log_level::debug:
-		color_code = "\x1b[34m";
-		break;
-	case log_level::warning:
-		color_code = "\x1b[33m";
-		break;
-	case log_level::error:
-		color_code = "\x1b[31m";
-		break;
-	case log_level::critical:
-		color_code = "\x1b[91m";
-		break;
-	default:
-		break;
-	}
-
-	fwrite(color_code, sizeof(char), strlen(color_code), stdout);
+    const char* get_ansi_color_code(dtlog::log_level level)
+    {
+        switch (level)
+        {
+        case dtlog::log_level::info:     return "\x1b[32m"; 
+        case dtlog::log_level::debug:    return "\x1b[36m"; 
+        case dtlog::log_level::warning:  return "\x1b[33m"; 
+        case dtlog::log_level::error:    return "\x1b[31m"; 
+        case dtlog::log_level::critical: return "\x1b[41m\x1b[37m"; 
+        case dtlog::log_level::trace:    return "\x1b[90m"; 
+        case dtlog::log_level::none:
+            break;
+        }
+        return "\x1b[0m";
+    }
 }
 
-void dtlog::logger::set_stderr_color(log_level level)
+#endif
+
+namespace dtlog
 {
-	const char* color_code = "\x1b[0m";
+    void console_sink::log(const std::string& msg, log_level level)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
 
-	switch (level)
-	{
-	case log_level::none:
-	case log_level::trace:
-		break;
-	case log_level::info:
-		color_code = "\x1b[32m";
-		break;
-	case log_level::debug:
-		color_code = "\x1b[34m";
-		break;
-	case log_level::warning:
-		color_code = "\x1b[33m";
-		break;
-	case log_level::error:
-		color_code = "\x1b[31m";
-		break;
-	case log_level::critical:
-		color_code = "\x1b[91m";
-		break;
-	default:
-		break;
-	}
+#ifdef _WIN32
+        set_console_color(STD_OUTPUT_HANDLE, level);
+        DTLOG_UNUSED(std::fwrite(msg.c_str(), 1, msg.length(), stdout));
+        reset_console_color(STD_OUTPUT_HANDLE);
+#else
+        const char* color_code = get_ansi_color_code(level);
+        const char* reset_code = "\x1b[0m";
+        
+        DTLOG_UNUSED(std::fwrite(color_code, 1, std::strlen(color_code), stdout));
+        DTLOG_UNUSED(std::fwrite(msg.c_str(), 1, msg.length(), stdout));
+        DTLOG_UNUSED(std::fwrite(reset_code, 1, 4, stdout));
+#endif
+        DTLOG_UNUSED(std::fflush(stdout));
+    }
 
-	fwrite(color_code, sizeof(char), strlen(color_code), stderr);
+    void err_console_sink::log(const std::string& msg, log_level level)
+    {
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+#ifdef _WIN32
+        set_console_color(STD_ERROR_HANDLE, level);
+        DTLOG_UNUSED(std::fwrite(msg.c_str(), 1, msg.length(), stderr));
+        reset_console_color(STD_ERROR_HANDLE);
+#else
+        const char* color_code = get_ansi_color_code(level);
+        const char* reset_code = "\x1b[0m";
+        
+        DTLOG_UNUSED(std::fwrite(color_code, 1, std::strlen(color_code), stderr));
+        DTLOG_UNUSED(std::fwrite(msg.c_str(), 1, msg.length(), stderr));
+        DTLOG_UNUSED(std::fwrite(reset_code, 1, 4, stderr));
+#endif
+        DTLOG_UNUSED(std::fflush(stderr));
+    }
 }
-
-#endif // _WIN32
